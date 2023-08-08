f802bb81eaea88d0bfb0309832567e5ce22a3633
aqbayadwala <codealot53@gmail.com>
2023-08-08 12:11:00 +0530

    tenth commit

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_required, login_user, logout_user
 from flask_login import UserMixin
-import os
-import psycopg2
+import pymysql
+
 
 # instantiate app
 app = Flask(__name__)
@@ -15,10 +15,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config[
     "DATABASE_URL"
-] = "postgresql://authen_9tnn_user:F8VZ73lmc0peyFXC4wjCpMH2HDDHjtQa@dpg-cj8lm0c5kgrc73b418s0-a.oregon-postgres.render.com/authen_9tnn"
-
-# instantiate Session
-Session(app)
+] = "postgres://authen_9tnn_user:F8VZ73lmc0peyFXC4wjCpMH2HDDHjtQa@dpg-cj8lm0c5kgrc73b418s0-a.oregon-postgres.render.com/authen_9tnn"
 
 # instantiate login manager
 login_manager = LoginManager()
@@ -32,27 +29,29 @@ def load_user(user_id):
     return User.get(user_id)
 
 
+# Database connection object
+connection_db = pymysql.connect(
+    host="dpg-cj8lm0c5kgrc73b418s0-a",
+    user="authen_9tnn_user",
+    password="F8VZ73lmc0peyFXC4wjCpMH2HDDHjtQa",
+    database="authen_9tnn",
+    port=5432,
+)
+
+
 # Define User Class
 class User(UserMixin):
     def __init__(self, id, username):
         self.id = id
         self.username = username
 
-    @classmethod
     def get(cls, user_id):
         user_query = "SELECT * FROM users WHERE id = %s"
         user_data = None
 
-        with psycopg2.connect(
-            host="dpg-cj8lm0c5kgrc73b418s0-a",
-            user="authen_9tnn_user",
-            password="F8VZ73lmc0peyFXC4wjCpMH2HDDHjtQa",
-            database="authen_9tnn",
-            port=5432,
-        ) as connection:
-            cursor = connection.cursor()
-            cursor.execute(user_query, user_id)
-            user_data = cursor.fetchone()
+        cursor = connection_db.cursor()
+        cursor.execute(user_query, user_id)
+        user_data = cursor.fetchone()
 
         if user_data:
             return cls(id=user_data[0], username=user_data[1])
@@ -64,16 +63,6 @@ class User(UserMixin):
 @app.route("/")
 def index():
     return render_template("home.html")
-
-
-# Database connection object
-connection_db = psycopg2.connect(
-    host="dpg-cj8lm0c5kgrc73b418s0-a",
-    user="authen_9tnn_user",
-    password="F8VZ73lmc0peyFXC4wjCpMH2HDDHjtQa",
-    database="authen_9tnn",
-    port=5432,
-)
 
 
 # Function for executing SQL queries
@@ -91,10 +80,19 @@ def register():
         username = request.form["username"]
         password = request.form["password"]
 
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS users (
+            id SMALLINT AUTO_INCREMENT PRIMARY KEY,
+            username CHAR(255) NOT NULL,
+            password_hash CHAR(255) NOT NULL
+        )
+        """
+        cursor = connection_db.cursor()
+        cursor.execute(create_table_query)
+
         existing_user_query = "SELECT * FROM users WHERE username = %s"
         existing_user_username = None
 
-        cursor = connection_db.cursor()
         cursor.execute(existing_user_query, (username,))
         existing_user_username = cursor.fetchone()
 
@@ -104,8 +102,12 @@ def register():
 
         hashed_password = generate_password_hash(password)
 
-        inser_user_query = "INSERT INTO users (username, password_hash) VALUES (%s, %s)"
-        execute_sql_query(inser_user_query, (username, hashed_password))
+        insert_user_query = (
+            "INSERT INTO users (username, password_hash) VALUES (%s, %s)"
+        )
+        execute_sql_query(insert_user_query, (username, hashed_password))
+        connection_db.commit()
+        connection_db.close()
         return render_template("registration_success.html")
 
     return render_template("register.html")
@@ -114,8 +116,6 @@ def register():
 # Login Route
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    session = Session(app)
-
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
@@ -130,8 +130,12 @@ def login():
         if user_data and check_password_hash(user_data[2], password):
             user = User(id=user_data[0], username=user_data[1])
             login_user(user)
+            connection_db.commit()
+            connection_db.close()
             return redirect("/dashboard")
         else:
+            connection_db.commit()
+            connection_db.close()
             not_valid = "Invalid User"
             return render_template("login.html", not_valid=not_valid)
     return render_template("login.html")
