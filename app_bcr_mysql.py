c2a8e8cc947d01cbc8894f7fa8d0eafce06bdc66
aqbayadwala <codealot53@gmail.com>
2023-08-10 15:01:12 +0530

    fresh day

--- app_bcr_mysql.py
+++ app_bcr_mysql.py
@@ -5,15 +5,16 @@ import os
 from flask_login import LoginManager, login_user, login_required, logout_user
 from flask_login import UserMixin
 
-# from flask_bcrypt import Bcrypt
-from argon2 import PasswordHasher
+from flask_bcrypt import Bcrypt
+
+# from argon2 import PasswordHasher
 
 app = Flask(__name__)
-# bcrypt = Bcrypt(app)
-ph = PasswordHasher()
+bcrypt = Bcrypt(app)
+# ph = PasswordHasher()
 
 # configurations
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("MYSQL_URL")
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 
@@ -32,12 +33,12 @@ login_manager.login_view = "login"
 # )
 
 # Database connection postgresql
-connection_db = psycopg2.connect(
-    host=os.environ.get("DB_HOST"),
-    port=int(os.environ.get("DB_PORT")),
-    user=os.environ.get("DB_USER"),
-    password=os.environ.get("DB_PASSWORD"),
-    database=os.environ.get("DB_NAME"),
+connection_db = pymysql.connect(
+    host=os.environ.get("MYSQLHOST"),
+    port=int(os.environ.get("MYSQLPORT")),
+    user=os.environ.get("MYSQLUSER"),
+    password=os.environ.get("MYSQLPASSWORD"),
+    database=os.environ.get("MYSQLDATABASE"),
 )
 
 
@@ -72,33 +73,33 @@ def index():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        username = request.form.get("username")
+        username = request.form["username"]
         # print(username)-debug
-        password = request.form.get("password")
+        password = request.form["password"]
         # bytes_register = password.encode("utf-8")
         # print(password)-debug
 
-        # create_table_query_mysql = """
-        #     CREATE TABLE IF NOT EXISTS users (
-        #         id SMALLINT(5) AUTO_INCREMENT PRIMARY KEY,
-        #         username CHAR(128) NOT NULL UNIQUE,
-        #         password_hash CHAR(128) NOT NULL
-        #     )
-        # """
-
-        create_table_query_postgresql = """
+        create_table_query_mysql = """
             CREATE TABLE IF NOT EXISTS users (
-                id SERIAL PRIMARY KEY,
-                username CHAR(255) NOT NULL UNIQUE,
-                password_hash CHAR(255) NOT NULL
+                id SMALLINT(5) AUTO_INCREMENT PRIMARY KEY,
+                username CHAR(128) NOT NULL UNIQUE,
+                password_hash CHAR(128) NOT NULL
             )
         """
+
+        # create_table_query_postgresql = """
+        #     CREATE TABLE IF NOT EXISTS users (
+        #         id SERIAL PRIMARY KEY,
+        #         username CHAR(255) NOT NULL UNIQUE,
+        #         password_hash CHAR(255) NOT NULL
+        #     )
+        # """
 
         existing_user_query = "SELECT * FROM users WHERE username=%s"
         existing_user_username = None
 
         cursor = connection_db.cursor()
-        cursor.execute(create_table_query_postgresql)
+        cursor.execute(create_table_query_mysql)
 
         cursor.execute(existing_user_query, (username,))
         existing_user_username = cursor.fetchone()
@@ -107,8 +108,8 @@ def register():
             error_message = "Username taken. Please choose a different username"
             return render_template("register.html", error_message=error_message)
 
-        hashed_password = ph.hash(password)
-        decoded_hashd_password = hashed_password
+        hashed_password = bcrypt.generate_password_hash(password)
+        decoded_hashd_password = hashed_password.decode("utf-8")
         print("hash while register: ", decoded_hashd_password)
 
         insert_user_query = (
@@ -125,32 +126,32 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        username_login = request.form.get("username")
-        password_login = request.form.get("password")
+        username_login = request.form["username"]
+        password_login = request.form["password"]
         # hashed_incoming = bcrypt.generate_password_hash(password_login)
         # bytes_login = password_login.encode("utf-8")
 
-        # create_table_query_mysql = """
-        #     CREATE TABLE IF NOT EXISTS users (
-        #         id SMALLINT(5) AUTO_INCREMENT PRIMARY KEY,
-        #         username CHAR(128) NOT NULL UNIQUE,
-        #         password_hash CHAR(128) NOT NULL
-        #     )
-        # """
-
-        create_table_query_postgresql = """
+        create_table_query_mysql = """
             CREATE TABLE IF NOT EXISTS users (
-                id SERIAL PRIMARY KEY,
-                username CHAR(255) NOT NULL UNIQUE,
-                password_hash CHAR(255) NOT NULL
+                id SMALLINT(5) AUTO_INCREMENT PRIMARY KEY,
+                username CHAR(128) NOT NULL UNIQUE,
+                password_hash CHAR(128) NOT NULL
             )
         """
+
+        # create_table_query_postgresql = """
+        #     CREATE TABLE IF NOT EXISTS users (
+        #         id SERIAL PRIMARY KEY,
+        #         username CHAR(255) NOT NULL UNIQUE,
+        #         password_hash CHAR(255) NOT NULL
+        #     )
+        # """
 
         user_query = "SELECT * FROM users WHERE username=%s"
         user_data = None
 
         cursor = connection_db.cursor()
-        cursor.execute(create_table_query_postgresql)
+        cursor.execute(create_table_query_mysql)
         cursor.execute(user_query, (username_login,))
         user_data = cursor.fetchone()
         hash = user_data[2]
@@ -159,7 +160,7 @@ def login():
         # print("Login Password Hash: ", hashed_incoming)
         # hash_bytes = hash.encode("utf-8")
         # print(hash_bytes)
-        check = ph.verify(hash, password_login)
+        check = bcrypt.check_password_hash(hash, password_login)
         print(check)
 
         if user_data and check:
